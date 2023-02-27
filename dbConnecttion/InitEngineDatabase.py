e028d937c41ad81e2943ea4805b799776249f12b
车小康 <35259787+lionche@users.noreply.github.com>
2023-02-27 15:13:23 +0800

    添加docker配置文件

--- dbConnecttion/InitEngineDatabase.py
+++ dbConnecttion/InitEngineDatabase.py
@@ -9,11 +9,12 @@ args = [('v8', '9.9.1','/root/.jsvu/v8',None),
         ('spiderMonkey', 'JavaScript-C96.0', '/root/.jsvu/spidermonkey', None),
         ('chakra', 'ch version 1.11.24.0', '/root/.jsvu/ch', None),
         ('jsc', 'v286936', '/root/.jsvu/javascriptcore', None),
-        ('quickjs', 'v2021-03-27', '/root/.jsvu/quickjs', None),
-        ('jerryscript', 'Version: 3.0.0 (fea10bb7)', '/root/.jsvu/jerry', None),
-        ('graaljs', '21.3.0', '/root/.jsvu/graaljs', None),
-        ('hermes', '0.10.0', '/root/.jsvu/hermes', None),
         ('chakra', 'ch version 1.13.0.0-beta', '/root/.jsvu/engines/chakra-1.13-cov/ch', None)]
+#
+# ('quickjs', 'v2021-03-27', '/root/.jsvu/quickjs', None),
+# ('jerryscript', 'Version: 3.0.0 (fea10bb7)', '/root/.jsvu/jerry', None),
+# ('graaljs', '21.3.0', '/root/.jsvu/graaljs', None),
+# ('hermes', '0.10.0', '/root/.jsvu/hermes', None),
 result = mysql.insertMany(sql,args)
 # print(result)
 
